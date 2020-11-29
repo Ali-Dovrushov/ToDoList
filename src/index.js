91c8d1fa5726f9ae47405146777e5b821d9f323f
Ali-Dovrushov <dovrushov7@gmail.com>
2020-11-29 15:58:11 +0400

    ToDoList

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+const total = document.querySelector('#total')
+let a = 0; 
+
+btn.addEventListener('click', (e) => {
+    if(input.value === ''){
+        return
+    }
+    createElementAndDelete(input.value)
+    input.value = ''
+})
+
+function createElementAndDelete(value) {
+    a++
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li'
+    li.textContent = value
+
+    li.addEventListener('click', (e) => {
+        li.classList.toggle('li-active')
+    })
+
+    total.textContent = a
+    li.appendChild(btn)
+    result.appendChild(li)
+}
